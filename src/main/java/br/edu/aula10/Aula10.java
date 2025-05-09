901d0615f1f140ebeb50d61dd31e8bdd56de47f7
Luiz Francisco <luizfk.marinho@gmail.com>
2025-05-09 18:03:26 -0300

    primeiro commit

---
+++ src/main/java/br/edu/aula10/Aula10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.aula10;
+
+import br.edu.aula10.view.Login;
+import br.edu.aula10.view.Principal;
+
+/**
+ *
+ * @author User
+ */
+public class Aula10 {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
---
+++ src/main/java/br/edu/aula10/model/Pessoa.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.aula10.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author User
+ */
+public class Pessoa {
+    
+    private static int valorId = 0;
+    private int id;
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNasc;
+    private String sexo;
+    private String pais;
+    private String estado;
+    private String cidade;
+
+    public Pessoa() {
+        
+    }
+
+    public Pessoa(int id, String nome, String sobrenome, LocalDate dataNasc, String sexo, String pais, String estado, String cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNasc = dataNasc;
+        this.sexo = sexo;
+        this.pais = pais;
+        this.estado = estado;
+        this.cidade = cidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        
+        this.id = valorId++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", dataNasc=" + dataNasc + ", sexo=" + sexo + ", pais=" + pais + ", estado=" + estado + ", cidade=" + cidade + '}';
+    }
+}
